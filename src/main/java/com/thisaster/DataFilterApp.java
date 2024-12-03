85521a9bdb8e64fcfcf8c33c3087e339c9abd49e
ThisAster <freizen034@mail.ru>
2024-12-03 11:12:39 +0300

    create: main class

--- src/main/java/com/thisaster/DataFilterApp.java
+++ src/main/java/com/thisaster/DataFilterApp.java
@@ -1,13 +1,19 @@
 package com.thisaster;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.thisaster.service.FilterService;
+import com.thisaster.util.ArgumentsParser;
+import org.apache.commons.cli.ParseException;
+
+public class DataFilterApp {
+    public static void main(String[] args) {
+        try {
+            ArgumentsParser arguments = new ArgumentsParser(args);
+            FilterService filterService = new FilterService(arguments);
+            filterService.processFiles(arguments.getInputFiles());
+        } catch (ParseException e) {
+            System.err.println("Error parsing arguments: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
