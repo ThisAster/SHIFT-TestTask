6eb8ac217fed1c61fddc244e324524576834ebf2
ThisAster <freizen034@mail.ru>
2024-12-03 11:12:07 +0300

    create: writer

--- src/main/java/com/thisaster/service/FileWriterService.java
+++ src/main/java/com/thisaster/service/FileWriterService.java
@@ -1,4 +1,19 @@
 package com.thisaster.service;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class FileWriterService {
+    public void writeData(String filepath, List<String> data, boolean append) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filepath, append))) {
+            for (String line : data) {
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println(("Error write data to file: " + e.getMessage()));
+        }
+    }
 }
